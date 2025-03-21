e0a9075f68a4ae00b24ee0ca57f626926a855068
Brinez2730 <125469635+Brinez2730@users.noreply.github.com>
2025-03-20 21:13:06 -0300

    Update atv1.py

--- atv1.py
+++ atv1.py
@@ -1,3 +1,8 @@
+#Peça ao usuário três notas e calcule:
+#a. A média aritmética simples
+#b. A média ponderada considerando os pesos 2, 2 e 3
+#c. A média ponderada considerando os pesos 1, 2 e 2
+
 n1 = int(input("Diga o primeiro numero: "))
 n2 = int(input("Diga o segundo numero: "))
 n3 = int(input("Diga o terceiro numero: "))
